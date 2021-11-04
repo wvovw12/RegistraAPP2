5fbc512987f7e53f731c59b860d754824edd0eb6
Diego Castro Garcés <invasivewand69@gmail.com>
2021-11-04 00:16:35 -0300

    Surrender?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, QRScanner],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/qr-page/qr-page.page.ts
+++ src/app/pages/qr-page/qr-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 @Component({
   selector: 'app-qr-page',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrPagePage implements OnInit {
 
-  constructor() { }
+  constructor(private qrScanner:QRScanner) { }
 
   ngOnInit() {
+
+    this.qrScanner.prepare().then((status: QRScannerStatus) => {
+     if (status.authorized) {
+      this.qrScanner.show();
+      (window.document.querySelector('ion-content') as HTMLElement).classList.add('background: none');
+
+       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+
+         this.qrScanner.hide();
+         scanSub.unsubscribe();
+         (window.document.querySelector('ion-content') as HTMLElement).classList.remove('background: none');
+       });
+
+     } else if (status.denied) {
+
+     } else {
+
+     }
+  })
+  .catch((e: any) => console.log('Error is', e));
   }
 
 }
+
